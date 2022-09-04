3640c1dcc3392d4fa836fddb67bbf525d69c02f4
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-09-04 14:35:47 +0200

    Adding XML docs

--- Nefarius.Standards.CAMT/054/Schema.cs
+++ Nefarius.Standards.CAMT/054/Schema.cs
@@ -3,80 +3,143 @@ using System.Collections.Generic;
 using System.Xml.Serialization;
 
 namespace Nefarius.Standards.CAMT._054;
-// using System.Xml.Serialization;
-// XmlSerializer serializer = new XmlSerializer(typeof(Document));
-// using (StringReader reader = new StringReader(xml))
-// {
-//    var test = (Document)serializer.Deserialize(reader);
-// }
 
+/// <summary>
+///     Message Recipient
+/// </summary>
 [XmlRoot(ElementName = "MsgRcpt")]
 public class MsgRcpt
 {
+    /// <summary>
+    ///     Name
+    /// </summary>
     [XmlElement(ElementName = "Nm")]
     public string Nm { get; set; }
 }
 
+/// <summary>
+///     Group Header
+/// </summary>
 [XmlRoot(ElementName = "GrpHdr")]
 public class GrpHdr
 {
+    /// <summary>
+    ///     Message Identification
+    /// </summary>
     [XmlElement(ElementName = "MsgId")]
     public double MsgId { get; set; }
 
+    /// <summary>
+    ///     Creation Date Time
+    /// </summary>
     [XmlElement(ElementName = "CreDtTm")]
     public DateTime CreDtTm { get; set; }
 
+    /// <summary>
+    ///     Message Recipient
+    /// </summary>
     [XmlElement(ElementName = "MsgRcpt")]
     public MsgRcpt MsgRcpt { get; set; }
 }
 
+/// <summary>
+///     Identification
+/// </summary>
 [XmlRoot(ElementName = "Id")]
 public class Id
 {
+    /// <summary>
+    ///     IBAN
+    /// </summary>
     [XmlElement(ElementName = "IBAN")]
     public string IBAN { get; set; }
 }
 
+/// <summary>
+///     Owner
+/// </summary>
 [XmlRoot(ElementName = "Ownr")]
 public class Ownr
 {
+    /// <summary>
+    ///     Name
+    /// </summary>
     [XmlElement(ElementName = "Nm")]
     public string Nm { get; set; }
+
+    /// <summary>
+    ///     Postal Address
+    /// </summary>
+    [XmlElement(ElementName = "PstlAdr")]
+    public string PstlAdr { get; set; }
 }
 
+/// <summary>
+///     Financial Institution Identification
+/// </summary>
 [XmlRoot(ElementName = "FinInstnId")]
 public class FinInstnId
 {
+    /// <summary>
+    ///     BIC
+    /// </summary>
     [XmlElement(ElementName = "BIC")]
     public string BIC { get; set; }
 }
 
+/// <summary>
+///     Servicer
+/// </summary>
 [XmlRoot(ElementName = "Svcr")]
 public class Svcr
 {
+    /// <summary>
+    ///     Financial Institution Identification
+    /// </summary>
     [XmlElement(ElementName = "FinInstnId")]
     public FinInstnId FinInstnId { get; set; }
 }
 
+/// <summary>
+///     Account
+/// </summary>
 [XmlRoot(ElementName = "Acct")]
 public class Acct
 {
+    /// <summary>
+    ///     Identification
+    /// </summary>
     [XmlElement(ElementName = "Id")]
     public Id Id { get; set; }
 
+    /// <summary>
+    ///     Currency
+    /// </summary>
     [XmlElement(ElementName = "Ccy")]
     public string Ccy { get; set; }
 
+    /// <summary>
+    ///     Owner
+    /// </summary>
     [XmlElement(ElementName = "Ownr")]
     public Ownr Ownr { get; set; }
 
+    /// <summary>
+    ///     Servicer
+    /// </summary>
     [XmlElement(ElementName = "Svcr")]
     public Svcr Svcr { get; set; }
 }
 
+/// <summary>
+///     Amount
+/// </summary>
 [XmlRoot(ElementName = "Amt")]
 public class Amt
 {
+    /// <summary>
+    ///     Currency
+    /// </summary>
     [XmlAttribute(AttributeName = "Ccy")]
     public string Ccy { get; set; }
 
@@ -84,134 +147,251 @@ public class Amt
     public int Text { get; set; }
 }
 
+/// <summary>
+///     Booking Date
+/// </summary>
 [XmlRoot(ElementName = "BookgDt")]
 public class BookgDt
 {
+    /// <summary>
+    ///     Date
+    /// </summary>
     [XmlElement(ElementName = "Dt")]
     public DateTime Dt { get; set; }
 }
 
+/// <summary>
+///     Value Date
+/// </summary>
 [XmlRoot(ElementName = "ValDt")]
 public class ValDt
 {
+    /// <summary>
+    ///     Date
+    /// </summary>
     [XmlElement(ElementName = "Dt")]
     public DateTime Dt { get; set; }
 }
 
+/// <summary>
+///     Family
+/// </summary>
 [XmlRoot(ElementName = "Fmly")]
 public class Fmly
 {
+    /// <summary>
+    ///     Code
+    /// </summary>
     [XmlElement(ElementName = "Cd")]
     public string Cd { get; set; }
 
+    /// <summary>
+    ///     Sub Family Code
+    /// </summary>
     [XmlElement(ElementName = "SubFmlyCd")]
     public string SubFmlyCd { get; set; }
 }
 
+/// <summary>
+///     Domain
+/// </summary>
 [XmlRoot(ElementName = "Domn")]
 public class Domn
 {
+    /// <summary>
+    ///     Code
+    /// </summary>
     [XmlElement(ElementName = "Cd")]
     public string Cd { get; set; }
 
+    /// <summary>
+    ///     Family
+    /// </summary>
     [XmlElement(ElementName = "Fmly")]
     public Fmly Fmly { get; set; }
 }
 
+/// <summary>
+///     Proprietary
+/// </summary>
 [XmlRoot(ElementName = "Prtry")]
 public class Prtry
 {
+    /// <summary>
+    ///     Code
+    /// </summary>
     [XmlElement(ElementName = "Cd")]
     public int Cd { get; set; }
 
+    /// <summary>
+    ///     Issuer
+    /// </summary>
     [XmlElement(ElementName = "Issr")]
     public string Issr { get; set; }
 }
 
+/// <summary>
+///     Bank Transaction Code
+/// </summary>
 [XmlRoot(ElementName = "BkTxCd")]
 public class BkTxCd
 {
+    /// <summary>
+    ///     Domain
+    /// </summary>
     [XmlElement(ElementName = "Domn")]
     public Domn Domn { get; set; }
 
+    /// <summary>
+    ///     Proprietary
+    /// </summary>
     [XmlElement(ElementName = "Prtry")]
     public Prtry Prtry { get; set; }
 }
 
+/// <summary>
+///     References
+/// </summary>
 [XmlRoot(ElementName = "Refs")]
 public class Refs
 {
+    /// <summary>
+    ///     Transaction Identification
+    /// </summary>
     [XmlElement(ElementName = "TxId")]
     public string TxId { get; set; }
 
+    /// <summary>
+    ///     End To End Identification
+    /// </summary>
     [XmlElement(ElementName = "EndToEndId")]
     public double EndToEndId { get; set; }
 }
 
+/// <summary>
+///     Transaction Amount
+/// </summary>
 [XmlRoot(ElementName = "TxAmt")]
 public class TxAmt
 {
+    /// <summary>
+    ///     Amount
+    /// </summary>
     [XmlElement(ElementName = "Amt")]
     public Amt Amt { get; set; }
 }
 
+/// <summary>
+///     Amount Details
+/// </summary>
 [XmlRoot(ElementName = "AmtDtls")]
 public class AmtDtls
 {
+    /// <summary>
+    ///     Transaction Amount
+    /// </summary>
     [XmlElement(ElementName = "TxAmt")]
     public TxAmt TxAmt { get; set; }
 }
 
+/// <summary>
+///     Postal Address
+/// </summary>
 [XmlRoot(ElementName = "PstlAdr")]
 public class PstlAdr
 {
+    /// <summary>
+    ///     Country
+    /// </summary>
     [XmlElement(ElementName = "Ctry")]
     public string Ctry { get; set; }
 
+    /// <summary>
+    ///     Address Line
+    /// </summary>
     [XmlElement(ElementName = "AdrLine")]
     public string AdrLine { get; set; }
 }
 
+/// <summary>
+///     Debtor
+/// </summary>
 [XmlRoot(ElementName = "Dbtr")]
 public class Dbtr
 {
+    /// <summary>
+    ///     Name
+    /// </summary>
     [XmlElement(ElementName = "Nm")]
     public string Nm { get; set; }
 
+    /// <summary>
+    ///     Postal Address
+    /// </summary>
     [XmlElement(ElementName = "PstlAdr")]
     public PstlAdr PstlAdr { get; set; }
 }
 
+/// <summary>
+///     Debtor Account
+/// </summary>
 [XmlRoot(ElementName = "DbtrAcct")]
 public class DbtrAcct
 {
+    /// <summary>
+    ///     Identification
+    /// </summary>
     [XmlElement(ElementName = "Id")]
     public Id Id { get; set; }
 }
 
+/// <summary>
+///     Creditor
+/// </summary>
 [XmlRoot(ElementName = "Cdtr")]
 public class Cdtr
 {
+    /// <summary>
+    ///     Name
+    /// </summary>
     [XmlElement(ElementName = "Nm")]
     public string Nm { get; set; }
 
+    /// <summary>
+    ///     Postal Address
+    /// </summary>
     [XmlElement(ElementName = "PstlAdr")]
     public PstlAdr PstlAdr { get; set; }
 }
 
+/// <summary>
+///     Related Parties
+/// </summary>
 [XmlRoot(ElementName = "RltdPties")]
 public class RltdPties
 {
+    /// <summary>
+    ///     Debtor
+    /// </summary>
     [XmlElement(ElementName = "Dbtr")]
     public Dbtr Dbtr { get; set; }
 
+    /// <summary>
+    ///     Debtor Account
+    /// </summary>
     [XmlElement(ElementName = "DbtrAcct")]
     public DbtrAcct DbtrAcct { get; set; }
 
+    /// <summary>
+    ///     Creditor
+    /// </summary>
     [XmlElement(ElementName = "Cdtr")]
     public Cdtr Cdtr { get; set; }
 }
 
+/// <summary>
+///     Debtor Agent
+/// </summary>
 [XmlRoot(ElementName = "DbtrAgt")]
 public class DbtrAgt
 {
